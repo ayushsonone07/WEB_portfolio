eccca07af3f175a146115cd995d2c1db4b4f809e
AnshSonone <sononeansh@gmail.com>
2024-03-05 18:54:01 +0530

    solved error

--- components/Swipes.jsx
+++ components/Swipes.jsx
@@ -8,7 +8,7 @@ import 'swiper/css/navigation';
 import { EffectFlip, Pagination, Navigation } from 'swiper/modules';
 import Image from 'next/image'
 import Link from 'next/link'
-import AWS_cloud from'@/public/certificates/AWS_cloud.jpg'
+import AWS_cloud from'@/public/certificates/AWS_Cloud.jpg'
 import AWS_ML from'@/public/certificates/AWS_ML.jpg'
 import cs50x from'@/public/certificates/cs50x.jpeg'
 import Intel_oneAPI from'@/public/certificates/Intel_oneAPI.jpeg'
